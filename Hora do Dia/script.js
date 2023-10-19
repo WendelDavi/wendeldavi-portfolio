3c424c9114b559c92657274c8a781683ade6de11
Wendel <wendeldavi777@gmail.com>
2023-10-18 21:01:20 -0300

    Commit inicial

---
+++ Hora do Dia/script.js
@@ -0,0 +1,19 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+    var horaFormatada = `${hora}:${minutos}`;
+    msg.innerHTML = `Agora são ${horaFormatada}.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'fotomanha.jpg'
+        document.body.style.background = 'rgb(78, 246, 237)'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'fototarde.jpg'
+        document.body.style.background = 'rgb(232, 162, 32)'
+    } else {
+        img.src = 'fotonoite.jpg'
+        document.body.style.background = 'rgb(52, 33, 179)'
+    }
+}
